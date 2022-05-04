0a31b5c380e709960d598d019ced74a9a5d51d8e
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2022-05-04 11:32:28 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt upgrade
+pip install SpeechRecognition
+pip install wolframalpha
+pip install pyttsx3
+pip install pyjokes
+pip install feedparser
+pip install twilio
+pip install ecapture
+pip install comtypes
+sudo apt-get install espeak
+pip install pipwin
+sudo apt-get install libportaudio-dev
+sudo apt-get install portaudio19-dev
+python3 -m pip install PyAudio
+sudo apt-get -y install python3-rpi.gpio
+
+echo "\nDone ...\n"
